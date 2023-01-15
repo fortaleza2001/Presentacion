165daf3b2cc1b61522d0222c179f35af1fddcbb0
Juan Carrasquer <juancarrasquer@gmail.com>
2023-01-15 07:22:40 +0100

    Primer coommit

---
+++ src/c/Juan.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package c;
+
+/**
+ *
+ * @author juanc
+ */
+public class Juan 
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Como va el trabajo de entornos?");
+    }
+}
